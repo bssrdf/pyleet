e55f51b7d3e5a2886469f516d5e92cae0b6bfe3c
bssrdf <merlintiger@hotmail.com>
2017-07-26 01:27:44 -0400

    added Fenwicktree

---
+++ FenwickTree.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 26 00:07:56 2017
+
+@author: merli
+"""
+
+class FenwickTree(object):
+    def __init__(self, n):
+        self.sum_array = [0] * (n + 1)
+        self.n = n
+ 
+    def lowbit(self, x):
+        return x & -x
+ 
+    def add(self, x, val):
+        while x <= self.n:
+            self.sum_array[x] += val
+            x += self.lowbit(x)
+ 
+    def sum(self, x):
+        res = 0
+        while x > 0:
+            res += self.sum_array[x]
+            x -= self.lowbit(x)
+        return res
+    
+if __name__ == "__main__":
+    n = 7
+    print bin(n)
+    print FenwickTree(10).lowbit(n)
+
+
+ 
