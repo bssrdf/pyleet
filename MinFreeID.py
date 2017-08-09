f02a2c4ba3d84d340f6af0e2720eac2e7646e879
bssrdf <bssrdf@gmail.com>
2017-08-08 20:02:48 -0400

    Min Free ID problem from AlgoXY added

---
+++ MinFreeID.py
@@ -0,0 +1,35 @@
+"""
+"""
+import bisect
+
+__author__ = 'Daniel'
+
+
+class Solution(object):    
+    
+    def helper(self, ids, l, r):
+        if l == r:
+            return l
+        m = l+(r-l)/2
+        left = l
+        for right in range(l, r+1):
+            if ids[right] <= m:
+                t = ids[left]
+                ids[left] = ids[right]
+                ids[right] = t
+                left += 1
+        if left == m+1:
+            l = m+1
+        else:
+            r = m
+        return self.helper(ids, l, r)        
+    
+    def MinFreeID(self, IDs):
+        return self.helper(IDs, 0, len(IDs)-1)        
+        
+
+if __name__ == "__main__":
+    #L = [18, 4, 8, 9, 16, 1, 14, 7, 19, 3, 0, 5, 2, 11, 6]
+    L = [10, 4, 8, 9, 1, 7, 3, 0, 5, 2, 11, 6]
+    print L
+    print Solution().MinFreeID(L)
