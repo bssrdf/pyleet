f385c8a5c4fb66dbba6be103c654f2eb0881c6e4
bssrdf <merlintiger@hotmail.com>
2017-08-10 01:26:53 -0400

    updated LongestSubstringAtMostTwoDistinctCharacters; fixed RemoveElement

--- LongestSubstringAtMostTwoDistinctCharacters.py
+++ LongestSubstringAtMostTwoDistinctCharacters.py
@@ -7,26 +7,55 @@ most two distinct characters.
 
 For example, Given S = “eceba”, T is "ece" which its length is 3.
 
+
+
 @author: merli
 """
 
 class Solution(object):
     def longestsubString(self, S):
         i, j, maxLen=0, -1, 0
+        # Use sliding window: maintain two pointers i, k
         for k in range(1, len(S)):
             if S[k] == S[k-1]:
                 continue
             if j >= 0 and S[j] != S[k]:
+                # max length is always max(k-i)
                 maxLen = max(k-i, maxLen)
-                i = j+1
-            print i, j, k, maxLen
+                i = j+1            
             j = k-1
-            
+            print i, j, k, maxLen
         #print j, i, maxLen
         return max(len(S)-i, maxLen)
 
+    def longestsubStringMap(self, S):
+        maxLen = 0
+        hmap={}        
+        i = 0
+        for k in range(len(S)):
+            if S[k] in hmap:
+                hmap[S[k]] += 1
+            else:
+                hmap[S[k]] = 1
+            if len(hmap) > 2:
+                maxLen = max(maxLen, k-i)
+                while len(hmap) > 2:
+                    count = hmap[S[i]]
+                    if count > 1:
+                        hmap[S[i]] -= 1
+                    else:
+                        del hmap[S[i]]
+                    i += 1
+            print i, k, maxLen
+        maxLen = max(maxLen, len(S)-i)
+        return maxLen
+                    
 
 
 if __name__ == "__main__":
    # assert Solution().longestsubString("eceba") == 3
-    assert Solution().longestsubString("abaac") == 4
+    #assert Solution().longestsubString("abaac") == 4
+    #assert Solution().longestsubString("abcdaaaaac") == 6
+    #assert Solution().longestsubString("cbaaaaaac") == 7
+    #assert Solution().longestsubStringMap("cbaaaaaac") == 7
+    assert Solution().longestsubStringMap("cbaaaaaa") == 7
--- RemoveElement.py
+++ RemoveElement.py
@@ -11,19 +11,21 @@ class Solution:
     # @param    elem    an integer, value need to be removed
     # @return an integer
     def removeElement(self, A, elem):
+        
         l = 0
-        r = len(A) - 1
+        r = len(A)
         while l < r:
-            if A[l] == elem:
-                while A[r] == elem:
-                    r -= 1
-                A[l] = A[r]
+            if A[l] == elem:              
                 r -= 1
-            l += 1
-        print A
-        return l+1
+                A[l], A[r] = A[r], A[l]
+            else:
+                l += 1
+       # print A
+        return r
     
 if __name__ == "__main__":
+    print Solution().removeElement([3, 3], 3)
+    print Solution().removeElement([4, 5], 4)
     print Solution().removeElement([1, 2, 3, 4, 5, 2, 2], 2)
     print Solution().removeElement([1, 2, 3, 4, 5, 2, 2], 10)
     print Solution().removeElement([1, 2, 3, 4, 5, 2, 2], 5)
