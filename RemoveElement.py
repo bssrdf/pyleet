191f7d7d506fe1a60e6b58d508b53129a7da6bc6
bssrdf <bssrdf@gmail.com>
2017-08-09 21:55:08 -0400

    RemoveElement added

---
+++ RemoveElement.py
@@ -0,0 +1,29 @@
+# Time:  O(n)
+# Space: O(1)
+#
+# Given an array and a value, remove all instances of that value in place and return the new length.
+# 
+# The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+#
+
+class Solution:
+    # @param    A       a list of integers
+    # @param    elem    an integer, value need to be removed
+    # @return an integer
+    def removeElement(self, A, elem):
+        l = 0
+        r = len(A) - 1
+        while l < r:
+            if A[l] == elem:
+                while A[r] == elem:
+                    r -= 1
+                A[l] = A[r]
+                r -= 1
+            l += 1
+        print A
+        return l+1
+    
+if __name__ == "__main__":
+    print Solution().removeElement([1, 2, 3, 4, 5, 2, 2], 2)
+    print Solution().removeElement([1, 2, 3, 4, 5, 2, 2], 10)
+    print Solution().removeElement([1, 2, 3, 4, 5, 2, 2], 5)
