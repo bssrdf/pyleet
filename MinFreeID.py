ede268eebe8f7cacce4d9a22bf5d3737a7c63006
bssrdf <merlintiger@hotmail.com>
2017-09-13 23:40:36 -0400

    fixed a bug in MinFreeID

--- MinFreeID.py
+++ MinFreeID.py
@@ -12,12 +12,14 @@ class Solution(object):
             return l
         m = l+(r-l)/2
         left = l
-        for right in range(l, r+1):
+        for right in range(l, r):
             if ids[right] <= m:
                 t = ids[left]
                 ids[left] = ids[right]
                 ids[right] = t
                 left += 1
+            print ids
+        print '*************************'
         if left == m+1:
             l = m+1
         else:
@@ -25,11 +27,11 @@ class Solution(object):
         return self.helper(ids, l, r)        
     
     def MinFreeID(self, IDs):
-        return self.helper(IDs, 0, len(IDs)-1)        
+        return self.helper(IDs, 0, len(IDs))        
         
 
 if __name__ == "__main__":
     #L = [18, 4, 8, 9, 16, 1, 14, 7, 19, 3, 0, 5, 2, 11, 6]
     L = [10, 4, 8, 9, 1, 7, 3, 0, 5, 2, 11, 6]
-    print L
+    print len(L)
     print Solution().MinFreeID(L)
