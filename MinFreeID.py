751ec3e2bca8b0cf57813a966966f4dd4b3cd964
bssrdf <bssrdf@gmail.com>
2017-10-08 20:44:02 -0400

    MinFreeID revised

--- MinFreeID.py
+++ MinFreeID.py
@@ -12,7 +12,7 @@ class Solution(object):
             return l
         m = l+(r-l)/2
         left = l
-        for right in range(l, r+1):
+        for right in range(l, r):
             if ids[right] <= m:
                 t = ids[left]
                 ids[left] = ids[right]
@@ -25,11 +25,11 @@ class Solution(object):
         return self.helper(ids, l, r)        
     
     def MinFreeID(self, IDs):
-        return self.helper(IDs, 0, len(IDs)-1)        
+        return self.helper(IDs, 0, len(IDs))        
         
 
 if __name__ == "__main__":
-    #L = [18, 4, 8, 9, 16, 1, 14, 7, 19, 3, 0, 5, 2, 11, 6]
-    L = [10, 4, 8, 9, 1, 7, 3, 0, 5, 2, 11, 6]
+    L = [18, 4, 8, 9, 16, 1, 14, 7, 19, 3, 0, 5, 2, 11, 6]
+    #L = [10, 4, 8, 9, 1, 7, 3, 0, 5, 2, 11, 6]
     print L
     print Solution().MinFreeID(L)
