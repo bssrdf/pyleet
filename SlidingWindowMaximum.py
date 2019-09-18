a11f0c0f05d17942085b9d4510ea239b03651468
bssrdf <merlintiger@hotmail.com>
2019-09-18 00:00:42 -0400

    updated solution explaination for SlidingWindowMaximum

--- SlidingWindowMaximum.py
+++ SlidingWindowMaximum.py
@@ -30,6 +30,24 @@ more information:
 https://medium.com/algorithms-and-leetcode/monotonic-queue-explained-with-leetcode-problems-7db7c530c1d6
 
 
+Solution Using Dequeue: O(N)
+
+Very similar code structure to heap solution
+http://yuanhsh.iteye.com/blog/2190852
+https://docs.python.org/2/library/collections.html#collections.deque
+Add to dequeue at tail using the rule where you pop all numbers from tail which are less than 
+equal to the number. Think why? 300->50->27 and say 100 comes. 50 and/or 27 can never be the 
+maximum in any range.
+When you do the above, the largest number is at head. But you still need to test if front is 
+within the range or not.
+Pop or push each element at-max once. O(N)
+*So, to maintain the queue in order,
+
+When moves to a new number, iterate through back of the queue, removes all numbers that are 
+not greater than the new one, and then insert the new one to the back.
+findMax only need to take the first one of the queue.
+To remove a number outside the window, only compare whether the current index is greater 
+than the front of queue. If so, remove it.*
 
 '''
 from collections import deque
