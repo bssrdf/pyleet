292443c7a185fc3081843c572b3c796e82d53220
bssrdf <merlintiger@hotmail.com>
2021-01-13 12:32:09 -0500

    marked problems solved in LC250

--- lc250.py
+++ lc250.py
@@ -1,10 +1,19 @@
 
+import os
 
-
+_, _, filenames = next(os.walk('./'))
+flist = [f.split('.')[0] for f in filenames if f.endswith('.py')]   
+print(flist[:10])
+flist = set((f.lower() for f in flist))
 with open('LC250.txt','r') as infile, open('LC250.md','w') as outfile:
     for line in infile.readlines():
         fields = line.split()
         #newfields ='- []'+'  '+fields[0]+'  '+''.join(fields[1:-1])+'\n' 
-        newfields ='- :heavy_check_mark: '+'  '+fields[0]+'  '+''.join(fields[1:-1])+'\n' 
-        #print(len(fields)) :heavy_multiplication_x:
+        prob_name = ''.join(fields[1:-1])
+        if prob_name.lower in flist:
+            newfields ='- :heavy_check_mark: '+'  '+fields[0]+'  '+prob_name+'\n' 
+        else:
+            newfields ='- :heavy_multiplication_x: '+'  '+fields[0]+'  '+prob_name+'\n' 
+        #print(len(fields)) 
         outfile.write(newfields)
+
