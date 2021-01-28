b5759f7742fd8fed7188eec239acb449d8cff268
bssrdf <merlintiger@hotmail.com>
2021-01-27 19:43:04 -0500

    add tags

--- LetterCombinationsOfAPhoneNumber.py
+++ LetterCombinationsOfAPhoneNumber.py
@@ -1,4 +1,8 @@
 '''
+-Medium-
+
+*DFS*
+
 Given a digit string, return all possible letter combinations that the number could represent.
 
 A mapping of digit to letters (just like on the telephone buttons) is given below.
