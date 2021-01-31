0f7be75b0586151c73b523625437934272ace181
bssrdf <merlintiger@hotmail.com>
2021-01-30 21:34:29 -0500

    added a solution using heap

--- MeetingRoomsII.py
+++ MeetingRoomsII.py
@@ -15,6 +15,7 @@ Output: 1
 '''
 
 import sys
+import heapq
 class Solution(object):
     def rooms(self, meetings):
         meets = sorted(meetings, key=lambda x: x[1])
@@ -25,7 +26,25 @@ class Solution(object):
                 n += 1
             e = m[1]
         return n
+        
+    def roomsUsingHeap(self, meetings):
+        meets = sorted(meetings, key=lambda x: x[0])
+        pq = [] 
+        cnt = 0      
+        for m in meets:
+            if not pq:
+                heapq.heappush(pq, m[1])
+                cnt += 1
+            else:
+                if m[0] >= pq[0]:
+                    heapq.heappop(pq)
+                else:
+                    cnt += 1
+                heapq.heappush(pq, m[1])
+        return cnt
 
 
 print(Solution().rooms([[0,30],[5,10],[15,20]]))
 print(Solution().rooms([[7,10],[2,4]]))
+print(Solution().roomsUsingHeap([[0,30],[5,10],[15,20]]))
+print(Solution().roomsUsingHeap([[7,10],[2,4]]))
