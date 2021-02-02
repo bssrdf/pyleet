b17a83eee3667497ea35eb4967694948d97f2045
bssrdf <merlintiger@hotmail.com>
2021-02-01 21:14:26 -0500

    one easy LC250 added

--- IPO.py
+++ IPO.py
@@ -2,6 +2,10 @@
 
 -Hard-
 
+*Heap*
+
+*Two Heaps*
+
 Suppose LeetCode will start its IPO soon. In order to sell a good price of its shares to 
 Venture Capital, LeetCode would like to work on some projects to increase its capital before 
 the IPO. Since it has limited resources, it can only finish at most k distinct projects 
