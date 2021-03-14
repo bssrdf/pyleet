3d90e06bc6fe7b53d225ddef58fc930e4175d60e
bssrdf <merlintiger@hotmail.com>
2021-03-14 16:25:50 -0400

    updated with a better version

--- SegmentTree.py
+++ SegmentTree.py
@@ -1,62 +1,114 @@
-class Node:
-    def __init__(self, left_side, right_side):
-        self.val = None
-        self.ls, self.rs = left_side, right_side
-        self.left_child, self.right_child = None, None
 
+#Segment tree node
+class Node(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.val = 0        
+        self.left = None
+        self.right = None
+        
 
-class SegmentTree:
-    def __init__(self, arr):
-        self.n = len(arr)
-        self.vals = arr[:]
-        self.root = self.build(0, self.n)
+class SegmentTree(object):
+    
+    def __init__(self, nums):
+        """
+        initialize your data structure here.
+        :type nums: List[int]
+        """
+        self.n = len(nums)
+        self.vals = nums[:]
+        self.root = self.createTree(nums, 0, len(nums)-1)
 
-    def build(self, l, r):
-        # 传入的l和r表示区间范围，左闭右开
-        if r - l < 1:
+    def createTree(self, nums, l, r):
+            
+        #base case
+        if l > r:
             return None
-        node = Node(l, r)
-        # 如果区间长度是1，说明是叶子节点了，直接将val赋值成对应的数值
-        if r - l == 1:
-            node.val = self.vals[l]
-        else:
-            # 否则递归调用
-            m = (l + r) >> 1
-            node.left_child = self.build(l, m)
-            node.right_child = self.build(m, r)
-            node.val = min(node.left_child.val, node.right_child.val)
-        return node
-    
-    def update(self, k, v):
-        self._update(self.root, k, v)
-
-    def _update(self, u, k, v):
-        if u is None:
-            return
-        # 如果k在u这个节点维护的区间里
-        if u.ls <= k < u.rs:
-            # 更新它的最小值
-            u.val = min(u.val, v)
-            m = (u.ls + u.rs) >> 1
-            # 判断往左还是往右
-            if k < m:
-                self._update(u.left_child, k, v)
+            
+        #leaf node
+        if l == r:
+            n = Node(l, r)
+            n.val = nums[l]
+            return n
+        
+        mid = (l + r) // 2
+        
+        root = Node(l, r)
+        
+        #recursively build the Segment tree
+        root.left = self.createTree(nums, l, mid)
+        root.right = self.createTree(nums, mid+1, r)
+        
+        #Total stores the sum of all leaves under root
+        #i.e. those elements lying between (start, end)
+        root.val = root.left.val + root.right.val
+            
+        return root
+        
+        
+            
+    def update(self, i, val):
+        """
+        :type i: int
+        :type val: int
+        :rtype: int
+        """
+        #Helper function to update a value
+        def updateVal(root, i, val):
+            
+            #Base case. The actual value will be updated in a leaf.
+            #The total is then propogated upwards
+            if root.start == root.end:
+                root.val = val
+                return val
+        
+            mid = (root.start + root.end) // 2
+            
+            #If the index is less than the mid, that leaf must be in the left subtree
+            if i <= mid:
+                updateVal(root.left, i, val)
+                
+            #Otherwise, the right subtree
             else:
-                self._update(u.right_child, k, v)
-    
-    def query(self, l, r):
-        return self._query(self.root, l, r)
+                updateVal(root.right, i, val)
+            
+            #Propogate the changes after recursive call returns
+            root.val = root.left.val + root.right.val
+            
+            return root.total
+        
+        return updateVal(self.root, i, val)
 
-    def _query(self, u, l, r):
-        # l和r是查询区间
-        # 如果查询区间是u节点区间的超集
-        if l <= u.ls and r >= u.rs:
-            return u.val
-        # 如果查询区间只和u节点区间的左半部分有交集
-        elif r <= u.left_child.rs:
-            return self._query(u.left_child, l, r)
-        # 如果查询区间只和u节点右半部分有交集
-        elif l >= u.right_child.ls:
-            return self._query(u.right_child, l, r)
-        # 如果都有交集
-        return min(self._query(u.left_child, l, r), self._query(u.right_child, l, r))
+    def query(self, i, j):
+        """
+        sum of elements nums[i..j], inclusive.
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        #Helper function to calculate range sum
+        def _query(root, i, j):
+            
+            #If the range exactly matches the root, we already have the sum
+            if root.start == i and root.end == j:
+                return root.val
+            
+            mid = (root.start + root.end) // 2
+            
+            #If end of the range is less than the mid, the entire interval lies
+            #in the left subtree
+            if j <= mid:
+                return _query(root.left, i, j)
+            
+            #If start of the interval is greater than mid, the entire inteval lies
+            #in the right subtree
+            elif i >= mid + 1:
+                return _query(root.right, i, j)
+            
+            #Otherwise, the interval is split. So we calculate the sum recursively,
+            #by splitting the interval
+            else:
+                return _query(root.left, i, mid) + _query(root.right, mid+1, j)
+        
+        return _query(self.root, i, j)
