0cf4e399e48cb4df02cd5320e5df04adddc3bde2
bssrdf <bssrdf@gmail.com>
2021-03-17 23:12:42 -0400

    another bit manip problem

---
+++ SumofTwoIntegers.py
@@ -0,0 +1,54 @@
+'''
+-Medium-
+
+*Bit Manipulation*
+
+Given two integers a and b, return the sum of the two integers without using the 
+
+operators + and -.
+
+ 
+
+Example 1:
+
+Input: a = 1, b = 2
+Output: 3
+Example 2:
+
+Input: a = 2, b = 3
+Output: 5
+ 
+
+Constraints:
+
+-1000 <= a, b <= 1000
+
+
+'''
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        # 32 bits integer max
+        MAX = 0x7FFFFFFF
+        # 32 bits interger min
+        MIN = 0x80000000
+        # mask to get last 32 bits
+        mask = 0xFFFFFFFF
+        while b != 0:
+            # ^ get different bits and & gets double 1s, << moves carry
+            a, b = (a ^ b) & mask, ((a & b) << 1) & mask
+        # if a is negative, get a's 32 bits complement positive first
+        # then get 32-bit positive's Python complement negative
+        return a if a <= MAX else ~(a ^ mask)
+
+if __name__ == "__main__":
+    #print(Solution().getSum(3,2))
+    #print(Solution().getSum(7,5))
+    print(Solution().getSum(-1,1))
+
+
+    
