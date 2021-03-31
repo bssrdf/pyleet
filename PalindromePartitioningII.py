68c8c9f10c8031149dd6a271caf281b2d76e89af
bssrdf <merlintiger@hotmail.com>
2021-03-30 22:30:57 -0400

    another LC250 problem

---
+++ PalindromePartitioningII.py
@@ -0,0 +1,57 @@
+'''
+-Hard-
+
+Given a string s, partition s such that every substring of the partition is a palindrome.
+
+Return the minimum cuts needed for a palindrome partitioning of s.
+
+ 
+
+Example 1:
+
+Input: s = "aab"
+Output: 1
+Explanation: The palindrome partitioning ["aa","b"] could be produced using 1 cut.
+Example 2:
+
+Input: s = "a"
+Output: 0
+Example 3:
+
+Input: s = "ab"
+Output: 1
+ 
+
+Constraints:
+
+1 <= s.length <= 2000
+s consists of lower-case English letters only.
+
+'''
+
+from collections import deque
+
+class Solution(object):
+    def minCut(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s: return 0
+        n = len(s)
+        dp = [float('inf')]*(n + 1)
+        dp[0] = -1
+        for i in range(n):
+            lth = 0
+            while i - lth >= 0 and i + lth < n and s[i - lth] == s[i + lth]:
+                dp[i + lth + 1] = min(dp[i + lth + 1], 1 + dp[i - lth])
+                lth += 1
+            lth = 0
+            while i - lth >= 0 and i + lth +1 < n and s[i - lth] == s[i + lth + 1]:
+                dp[i + lth + 2] = min(dp[i + lth + 2], 1 + dp[i - lth])
+                lth += 1
+        return dp[n]         
+
+        
+if __name__ == "__main__":
+    print(Solution().minCut("aab"))
