db16a2cd13072dccc8e1139d031fb202d4b63a05
bssrdf <bssrdf@gmail.com>
2021-04-03 09:22:58 -0400

    updated with a DP (faster) solution

--- LongestValidParentheses.py
+++ LongestValidParentheses.py
@@ -1,4 +1,8 @@
 '''
+-Hard-
+*Stack*
+*DP*
+
 Given a string containing just the characters '(' and ')', find the length 
 of the longest valid (well-formed) parentheses substring.
 
@@ -34,9 +38,7 @@ class Solution(object):
         :rtype: int
         """
         st = []
-        n = len(s)        
-        res = 0
-        start = 0
+        res, start, n = 0, 0, len(s)        
         for i in range(n):
             if s[i] == '(': st.append(i)
             if s[i] == ')':
@@ -48,8 +50,44 @@ class Solution(object):
                         res = max(res, i-start+1)
                     else:
                         res = max(res, i-st[-1])
-        #print('start',start)
+            #print('start',start, 'res', res)
         return res
+    
+    def longestValidParenthesesDP(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        """
+        If s[i] is '(', set longest[i] to 0,because any string end with '(' cannot 
+        be a valid one.
+
+        Else if s[i] is ')'
+
+            If s[i-1] is '(', longest[i] = longest[i-2] + 2
+
+            Else if s[i-1] is ')' and s[i-longest[i-1]-1] == '(', 
+                longest[i] = longest[i-1] + 2 + longest[i-longest[i-1]-2]
+
+            For example, input "()(())", at i = 5, longest array is [0,2,0,0,2,0], 
+            longest[5] = longest[4] + 2 + longest[1] = 6.
+
+        """
+        n = len(s)
+        if len(s) == 1: return 0
+        dp = [0]*n
+        res = 0
+        for i in range(1, n):
+            if s[i] == '(': continue # cannot form the valid parentheses
+            if s[i-1] == '(':
+                dp[i] = dp[i-2]+2 if i >= 2 else 2
+            else:
+                if i-dp[i-1]-1 >= 0 and s[i-1-dp[i-1]] == '(':
+                    dp[i] = dp[i-1] + 2 + (dp[i-dp[i-1]-2] if i-dp[i-1]-2>=0 else 0)
+            res = max(res, dp[i])
+        return res
+
+                 
 
 
         
@@ -60,11 +98,12 @@ class Solution(object):
         
 
 if __name__ == "__main__":
-    #'''
+    '''
     print(Solution().longestValidParentheses("()())()"))
     print(Solution().longestValidParentheses("(()"))
     print(Solution().longestValidParentheses(")()())"))
     print(Solution().longestValidParentheses(""))
-    #'''
+    '''
     print(Solution().longestValidParentheses("()(()"))
     print(Solution().longestValidParentheses("(((())"))
+    print(Solution().longestValidParenthesesDP("(((())"))
