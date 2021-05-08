54b1580e1dfe035cbce2c48f851a1d75547ca6d9
bssrdf <merlintiger@hotmail.com>
2021-05-07 22:28:33 -0400

    two longest common subsequence problems

---
+++ DeleteOperationforTwoStrings.py
@@ -0,0 +1,48 @@
+'''
+-Medium-
+
+Given two strings word1 and word2, return the minimum number of steps required to make word1 
+and word2 the same.
+
+In one step, you can delete exactly one character in either string.
+
+ 
+
+Example 1:
+
+Input: word1 = "sea", word2 = "eat"
+Output: 2
+Explanation: You need one step to make "sea" to "ea" and another step to make "eat" to "ea".
+Example 2:
+
+Input: word1 = "leetcode", word2 = "etco"
+Output: 4
+ 
+
+Constraints:
+
+1 <= word1.length, word2.length <= 500
+word1 and word2 consist of only lowercase English letters.
+
+
+'''
+
+class Solution(object):
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        m, n = len(word1), len(word2)
+        dp = [[0]*(n+1) for _ in range(m+1)]
+        for i in range(1, m+1):            
+            for j in range(1, n+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        return m+n-dp[m][n]*2
+
+if __name__ == "__main__":
+    print(Solution().minDistance(word1 = "leetcode", word2 = "etco"))
