7e12823a2c2fb8feb64b72ed7a98cf120b195d22
bssrdf <bssrdf@gmail.com>
2021-05-23 08:56:57 -0400

    unfinished

---
+++ FindtheShortestSuperstring.py
@@ -0,0 +1,40 @@
+'''
+-Hard-
+*DP*
+Given an array of strings words, return the smallest string that contains each 
+string in words as a substring. If there are multiple valid strings of the 
+smallest length, return any of them.
+
+You may assume that no string in words is a substring of another string in words.
+
+ 
+
+Example 1:
+
+Input: words = ["alex","loves","leetcode"]
+Output: "alexlovesleetcode"
+Explanation: All permutations of "alex","loves","leetcode" would also be accepted.
+Example 2:
+
+Input: words = ["catg","ctaagt","gcta","ttca","atgcatc"]
+Output: "gctaagttcatgcatc"
+ 
+
+Constraints:
+
+1 <= words.length <= 12
+1 <= words[i].length <= 20
+words[i] consists of lowercase English letters.
+All the strings of words are unique.
+
+'''
+
+class Solution(object):
+    def shortestSuperstring(self, words):
+        """
+        :type words: List[str]
+        :rtype: str
+        """
+
+if __name__ == "__main__":
+    print(Solution().shortestSuperstring(["catg","ctaagt","gcta","ttca","atgcatc"]))
