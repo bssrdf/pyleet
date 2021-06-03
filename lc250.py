400bfcb595354f03ac382d13378461cc3675d84f
bssrdf <merlintiger@hotmail.com>
2021-06-03 13:15:40 -0400

    LC250 board updated

--- lc250.py
+++ lc250.py
@@ -5,12 +5,8 @@ _, _, filenames = next(os.walk('./'))
 flist = [f.split('.')[0] for f in filenames if f.endswith('.py')]   
 flist = set((f.lower() for f in flist))
 finished = 0
-with open('LC250.md','w') as outfile:
-    outfile.write("# Leetcode 前 400 重点 250 题")    
-    outfile.write("\n\n")    
-    outfile.write("---")    
-    outfile.write("\n\n")    
-with open('LC250.txt','r') as infile, open('LC250.md','a') as outfile:
+newfields = []
+with open('LC250.txt','r') as infile:
     for line in infile.readlines():
         fields = line.split()        
         prob_name = ''.join(fields[1:-1])
@@ -21,10 +17,24 @@ with open('LC250.txt','r') as infile, open('LC250.md','a') as outfile:
             mark = ':heavy_check_mark:'
         else:
             mark = ':heavy_multiplication_x:'
-        newfields ='- '+mark+'  '+fields[0]+'  '+prob_name+'\n' 
-        outfile.write(newfields)
+        #newfields ='- '+mark+'  '+fields[0]+'  '+prob_name+'\n' 
+        newfields.append('- '+mark+'  '+fields[0]+'  '+prob_name+'\n') 
+        #outfile.write(newfields)
 summary = "\n## **%d** have been finished with **%d** left" %(finished, 250-finished)
-with open('LC250.md','a') as outfile:
-    outfile.write("\n---")
+with open('LC250.md','w') as outfile:
+    outfile.write("# Leetcode 前 400 重点 250 题")    
+    outfile.write("\n\n")    
+    outfile.write("---")    
+    outfile.write("\n\n")    
     outfile.write(summary)
+    outfile.write("\n---")
+    for line in newfields:
+        outfile.write(line)
+
+
+
+
+#with open('LC250.md','a') as outfile:
+#    outfile.write("\n---")
+#    outfile.write(summary)
 print(summary)
