08847e7a220aa18b738579ec4e70d9a84fc04b00
bssrdf <merlintiger@hotmail.com>
2021-06-07 22:28:55 -0400

    a easy challenge problem

---
+++ MinCostClimbingStairs.py
@@ -0,0 +1,69 @@
+'''
+-Easy-
+*DP*
+
+
+You are given an integer array cost where cost[i] is the cost of ith step on a staircase. 
+Once you pay the cost, you can either climb one or two steps.
+
+You can either start from the step with index 0, or the step with index 1.
+
+Return the minimum cost to reach the top of the floor.
+
+ 
+
+Example 1:
+
+Input: cost = [10,15,20]
+Output: 15
+Explanation: Cheapest is: start on cost[1], pay that cost, and go to the top.
+Example 2:
+
+Input: cost = [1,100,1,1,1,100,1,1,100,1]
+Output: 6
+Explanation: Cheapest is: start on cost[0], and only step on 1s, skipping cost[3].
+ 
+
+Constraints:
+
+2 <= cost.length <= 1000
+0 <= cost[i] <= 999
+
+'''
+
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        #                           _________________
+        #                      ___ | Final step, Top
+        #                 ___ | 20
+        #            ___ | 15
+        # _________ | 10
+        # First step
+        # Say f[i] is the final cost to climb to the top from step i. 
+        # Then f[i] = cost[i] + min(f[i+1], f[i+2]).
+        n = len(cost)
+        '''
+        dp = [0]*n
+        dp[n-1] = cost[n-1]
+        dp[n-2] = cost[n-2]
+        for i in range(n-3,-1,-1):
+            dp[i] = min(dp[i+1], dp[i+2]) + cost[i]            
+        return min(dp[0], dp[1])
+        '''        
+        dp_i2 = cost[n-1]
+        dp_i1 = cost[n-2]
+        do_i = 0
+        for i in range(n-3,-1,-1):
+            dp_i = min(dp_i1, dp_i2) + cost[i]            
+            dp_i2 = dp_i1
+            dp_i1 = dp_i
+        return min(dp_i1, dp_i2)
+
+
+if __name__=="__main__":
+    print(Solution().minCostClimbingStairs([1,100,1,1,1,100,1,1,100,1]))
+    print(Solution().minCostClimbingStairs([10,15,20]))
