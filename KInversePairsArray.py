4bd9b919663b0916428b7790bb3eb5cd40bfc75d
bssrdf <bssrdf@gmail.com>
2021-06-20 18:42:06 -0400

    updated with explanation

--- KInversePairsArray.py
+++ KInversePairsArray.py
@@ -55,6 +55,34 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        """
+        dp[n][k] denotes the number of arrays that have k inverse pairs for array composed 
+        of 1 to n, we can establish the recursive relationship between dp[n][k] and dp[n-1][k]:
+
+        if we put n as the last number then all the k inverse pair should come from the 
+        first n-1 numbers
+
+        if we put n as the second last number then there's 1 inverse pair involves n so 
+        the rest k-1 comes from the first n-1 numbers
+        ...
+        if we put n as the first number then there's n-1 inverse pairs involve n so the 
+        rest k-(n-1) comes from the first n-1 numbers
+
+        dp[n][k] = dp[n-1][k]+dp[n-1][k-1]+dp[n-1][k-2]+...+dp[n-1][k+1-n+1]+dp[n-1][k-n+1]
+
+        It's possible that some where in the right hand side the second array index 
+        become negative, since we cannot generate negative inverse pairs we just treat 
+        them as 0, but still leave the item there as a place holder.
+
+        dp[n][k] = dp[n-1][k]+dp[n-1][k-1]+dp[n-1][k-2]+...+dp[n-1][k+1-n+1]+dp[n-1][k-n+1]
+        dp[n][k+1] = dp[n-1][k+1]+dp[n-1][k]+dp[n-1][k-1]+dp[n-1][k-2]+...+dp[n-1][k+1-n+1]
+
+        so by deducting the first line from the second line, we have
+
+        dp[n][k+1] = dp[n][k]+dp[n-1][k+1]-dp[n-1][k+1-n]
+
+
+        """
         dp = [[0 for _ in range(k+1)] for _ in range(n+1)]
         MOD = 10**9 +7
         dp[0][0] = 1
