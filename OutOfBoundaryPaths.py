47827281ac913421210ee8f4674a0a686cc3107d
bssrdf <bssrdf@gmail.com>
2021-06-24 10:15:39 -0400

    challenge problem updated

--- OutOfBoundaryPaths.py
+++ OutOfBoundaryPaths.py
@@ -36,7 +36,8 @@ N is in range [0,50].
 '''
 
 class Solution(object):
-    def findPaths(self, m, n, N, i, j):
+    #def findPaths(self, m, n, N, i, j):
+    def findPaths(self, m, n, maxMove, startRow, startColumn):
         """
         :type m: int
         :type n: int
@@ -45,24 +46,26 @@ class Solution(object):
         :type j: int
         :rtype: int
         """
-        if N == 0: return 0
+        if maxMove == 0: return 0
         if m == 1 and n == 1 : return 4
         dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]                
-        memo = [[[-1 for _ in range(N)] for _ in range(n)] for _ in range(m)]  
+        memo = [[[-1 for _ in range(maxMove)] for _ in range(n)] for _ in range(m)]  
         def dfs(i,j,k):
             if i < 0 or i > m-1 or j < 0 or j > n-1:
                 return 1
-            if k == N:
+            if k == maxMove:
                 return 0
             if memo[i][j][k] != -1:
                 return memo[i][j][k]   
             ib = 0    
             for di, dj in dirs:
                 ib += dfs(i+di, j+dj, k+1)                
-            memo[i][j][k] = ib                
+            memo[i][j][k] = ib 
+            print(i,j,k,memo[i][j][k])               
             return memo[i][j][k]       
-        return dfs(i,j,0)
+        return dfs(startRow,startColumn,0)% (10**9+7)
 
 if __name__ == "__main__":
     print(Solution().findPaths(2, 2, 2, 0, 0))
     print(Solution().findPaths(1, 3, 3, 0, 1))
+    print(Solution().findPaths(3, 2, 5, 1, 1))
