bcfd3eb00189869e1b708797b090c8d8884208b1
bssrdf <merlintiger@hotmail.com>
2021-07-28 22:47:19 -0400

    added comments for better undersstanding

--- HopcroftKarpBipartiteMatching.py
+++ HopcroftKarpBipartiteMatching.py
@@ -47,7 +47,8 @@ class BipGraph(object):
         # there is an augmenting path.
         k = 0
         while self.bfs():
-            print('before dfs', self.dist)
+            print('before dfs, dist = ', self.dist)
+            print('before dfs, pairU = ', self.pairU)
             # Find a free vertex
             for u in range(1, self.m+1):
                 # If current vertex is free and there is
@@ -58,7 +59,7 @@ class BipGraph(object):
             print('After iteration ', k)
             print('PairU : ',self.pairU)
             print('PairV : ', self.pairV)
-            print(self.dist)
+            print('after dfs, dist = ',self.dist)
             print('matchings: ', result)
         return result
  
@@ -81,12 +82,14 @@ class BipGraph(object):
             # Else set distance as infinite
             # so that this vertex is
             # considered next time
-            else:
+            else: # if this vertex has been matched, set dist to INF
                 self.dist[u] = INF
  
         # Initialize distance to
         # NIL as infinite
         self.dist[NIL] = INF
+
+        print("size of Q: ", len(Q))
  
         # Q is going to contain vertices
         # of left side only.
@@ -106,16 +109,22 @@ class BipGraph(object):
                     # If pair of v is not considered
                     # so far (v, pairV[V]) is not yet
                     # explored edge.
+                    # we require in our BFS that when going from V to U, 
+                    # we always select a matched edge. 
                     if self.dist[self.pairV[v]] == INF:
                          
                         # Consider the pair and add
                         # it to queue
+                        #if self.pairV[v] == NIL: 
+                         #   print('u,v', u, v, self.dist[u])
                         self.dist[self.pairV[v]] = self.dist[u] + 1
                         Q.append(self.pairV[v])
  
         # If we could come back to NIL using
         # alternating path of distinct vertices
         # then there is an augmenting path
+        # If no path has been found, then there are no augmenting paths left 
+        # and the matching is maximal. bfs returns false to terminate the while loop
         return self.dist[NIL] != INF
  
     # Returns true if there is an augmenting
@@ -125,10 +134,16 @@ class BipGraph(object):
             for v in self.adj[u]:
  
                 # Follow the distances set by BFS
+                # Note that the code ensures that all augmenting paths 
+                # that we consider are vertex disjoint. Indeed, after 
+                # doing the symmetric difference for a path, none of its 
+                # vertices could be considered again in the DFS, just because 
+                # the Dist[Pair_V[v]] will not be equal to Dist[u] + 1 (it would be exactly Dist[u]).
                 if self.dist[self.pairV[v]] == self.dist[u] + 1:
                      
                     # If dfs for pair of v also returns
                     # true
+                    print(u, v, self.pairV[v], self.dist[self.pairV[v]], self.dist[u] )
                     if self.dfs(self.pairV[v]):
                         self.pairV[v] = u
                         self.pairU[u] = v
@@ -136,6 +151,9 @@ class BipGraph(object):
  
             # If there is no augmenting path
             # beginning with u.
+            # When we were not able to find any shortest augmenting path from a vertex u, 
+            # then the DFS marks vertex u by setting Dist[u] to infinity, so that these 
+            # vertices are not visited again.
             self.dist[u] = INF
             return False
         return True
@@ -144,16 +162,17 @@ class BipGraph(object):
  
 #Driver code
 if __name__ == "__main__":
-     
+    # the graph is from the link below
+    # https://en.wikipedia.org/wiki/Hopcroft%E2%80%93Karp_algorithm#/media/File:HopcroftKarpExample.png 
     g = BipGraph(5, 5);
     g.addEdge(1, 1);
     g.addEdge(1, 2);
     g.addEdge(2, 1);
-    g.addEdge(2, 5);
+    #g.addEdge(2, 5);
     g.addEdge(3, 3);
-    g.addEdge(3, 4);
+    #g.addEdge(3, 4);
     g.addEdge(4, 1);
-    g.addEdge(4, 5);
+    #g.addEdge(4, 5);
     g.addEdge(5, 2);
     g.addEdge(5, 4);
  
