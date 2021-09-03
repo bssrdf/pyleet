055f0df74112664d8338f4456ed603f154c79d14
bssrdf <merlintiger@hotmail.com>
2021-09-02 21:39:21 -0400

    updated with a new solution

--- SortFeaturesbyPopularity.py
+++ SortFeaturesbyPopularity.py
@@ -4,18 +4,17 @@
 *Bit Manipulation*
 *Sort*
 
-You are given a string array features where features[i] is a single word representing the name 
-of a feature of the latest product you are working on. You made a survey where users reported 
-which features they like. You are given a string array responses, where responses[i] is a string 
+You are given a string array features where features[i] is a single word that represents the name of 
+a feature of the latest product you are working on. You have made a survey where users have reported 
+which features they like. You are given a string array responses, where each responses[i] is a string 
 containing space-separated words.
 
-You want to sort the features according to their popularity. More formally, let appearances(word) 
-be the number of is such that responses[i] contains word as a word. Then the x-th feature is 
-more popular than the y-th feature if appearances(features[x]) > appearances(features[y]).
+The popularity of a feature is the number of responses[i] that contain the feature. You want to sort 
+the features in non-increasing order by their popularity. If two features have the same popularity, 
+order them by their original index in features. Notice that one response could contain the same 
+feature multiple times; this feature is only counted once in its popularity.
 
-Return an array sortedFeatures consisting of the feature names sorted by their popularity. If 
-the x-th and y-th features have the same popularity where x < y, then you should put the x-th 
-feature before the y-th one.
+Return the features in sorted order.
 
 Example 1:
 
@@ -45,7 +44,7 @@ responses[i] has no leading or trailing spaces.
 
 
 '''
-import heapq
+from collections import defaultdict
 class Solution(object):
     def sortedFeature(self, features, responses):
         m = {}
@@ -70,13 +69,29 @@ class Solution(object):
         pairs.sort()
         return [p[1] for p in pairs]
 
+    def sortedFeature2(self, features, responses):
+        m = defaultdict(set)
+        for i,res in enumerate(responses):
+            for w in res.split():
+                m[w].add(i)
+        pairs = []
+        for w in features:
+            if w in m:
+                pairs.append((-len(m[w]),w))
+            else:
+                pairs.append((0,w))
+        pairs.sort()
+        return [p[1] for p in pairs]
+
 if __name__=="__main__":
     features = ["cooler","lock","touch"]
     responses = ["i like cooler cooler","lock touch cool","locker like touch"]
     print(Solution().sortedFeature(features, responses))
+    print(Solution().sortedFeature2(features, responses))
     features = ['a','aa','b','c'] 
     responses = ['a','a aa','a a a a a','b a']
     print(Solution().sortedFeature(features, responses))
+    print(Solution().sortedFeature2(features, responses))
 
 
 
