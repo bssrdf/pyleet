8e9ab75a9c975865a61476af88a8fa9b6b6c179f
bssrdf <merlintiger@hotmail.com>
2021-11-13 22:40:04 -0500

    2 new and 1 old revisited

---
+++ BeforeandAfterPuzzle.py
@@ -0,0 +1,86 @@
+'''
+
+-Medium-
+*Hash Table*
+*Sorting*
+
+Given a list of phrases, generate a list of Before and After puzzles.
+
+A phrase is a string that consists of lowercase English letters and spaces only. 
+No space appears in the start or the end of a phrase. There are no consecutive 
+spaces in a phrase.
+
+Before and After puzzles are phrases that are formed by merging two phrases 
+where the last word of the first phrase is the same as the first word of the 
+second phrase.
+
+Return the Before and After puzzles that can be formed by every two phrases 
+phrases[i] and phrases[j] where i != j. Note that the order of matching two 
+phrases matters, we want to consider both orders.
+
+You should return a list of distinct strings sorted lexicographically.
+
+Example 1:
+
+Input: phrases = ["writing code","code rocks"]
+Output: ["writing code rocks"]
+Example 2:
+
+Input: phrases = ["mission statement",
+                  "a quick bite to eat",
+                  "a chip off the old block",
+                  "chocolate bar",
+                  "mission impossible",
+                  "a man on a mission",
+                  "block party",
+                  "eat my words",
+                  "bar of soap"]
+Output: ["a chip off the old block party",
+         "a man on a mission impossible",
+         "a man on a mission statement",
+         "a quick bite to eat my words",
+         "chocolate bar of soap"]
+Example 3:
+
+Input: phrases = ["a","b","a"]
+Output: ["a"]
+Constraints:
+
+1 <= phrases.length <= 100
+1 <= phrases[i].length <= 100
+
+
+'''
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def beforeAndAfterPuzzles(self, phrases: List[str]) -> List[str]:
+        head, tail = defaultdict(list), defaultdict(list) 
+        res = set()
+        for i, phrase in enumerate(phrases):
+            words = phrase.split()            
+            head[words[0]].append(i)
+            tail[words[1]].append(i)
+        for phrase in phrases:
+            words = phrase.split()
+            if words[0] in tail:
+                for i in tail[words[0]]:
+                    res.add(phrases[i]+' '+ ' '.join(words[1:]))
+            if words[-1] in head:
+                for i in head[words[-1]]:
+                    res.add(' '.join(words[:-1])+' '+phrases[i])
+        return sorted(list(res))
+       
+
+if __name__ == "__main__":
+    phrases = ["mission statement",
+                  "a quick bite to eat",
+                  "a chip off the old block",
+                  "chocolate bar",
+                  "mission impossible",
+                  "a man on a mission",
+                  "block party",
+                  "eat my words",
+                  "bar of soap"]
+    print(Solution().beforeAndAfterPuzzles(phrases))
--- IteratorforCombination.py
+++ IteratorforCombination.py
@@ -50,19 +50,22 @@ class CombinationIterator(object):
         self.bitMask = 0
         self.masks = []
         self.idx = 0
-        def dfs(s, pos, k):            
+        def dfs(pos, k):       
+  #          print('A', pos, k, bin(self.bitMask))
             if k == self.length: 
                 self.bitMask |= 1 << pos
                 self.masks.append(self.bitMask)
                 self.bitMask &= ~(1 << pos)    
                 return
             self.bitMask |= 1 << pos
-            #for i in range(pos+1, len(s)):
-            for i in range(pos+1, len(s)-(self.length-k)+1):
-                dfs(s, i, k+1)
+            #for i in range(pos, len(self.s)):
+            for i in range(pos+1, len(self.s)-(self.length-k)+1):
+            #for i in range(pos, len(self.s)-k+1):
+                dfs(i, k+1)
             self.bitMask &= ~(1 << pos)    
         for i in range(len(self.s)-self.length+1):
-            dfs(self.s, i, 1)
+            dfs(i, 1)
+        #print(self.masks)
 
     def next(self):
         """
@@ -84,10 +87,69 @@ class CombinationIterator(object):
         :rtype: bool
         """
         return self.idx < len(self.masks)
+        
+class CombinationIterator2(object):
+    def __init__(self, characters, combinationLength):
+        """
+        :type characters: str
+        :type combinationLength: int
+        """
+        self.s = characters
+        self.combLength = combinationLength
+        self.ch2Idx = {}
+        self.st = []
+        idx = 0
+        self.result = ''
+        for c in characters:
+            self.st.append(c)
+            self.result += c
+            if len(self.result) == self.combLength:
+                break
+        for c in characters:
+            self.ch2Idx[c] = idx
+            idx += 1
 
+    def next(self):
+        """
+        :rtype: str
+        """
+        currResult = self.result
+        idx = len(self.s) - 1
+        # keep on removing the last character from the stack/result till the position where idx can be moved ahead
+        while self.st and self.ch2Idx[self.st[-1]] == idx: 
+            self.st.pop()
+            idx -= 1
+            self.result = self.result[:-1]
+        if not self.st: 
+            print(currResult, self.result, self.st)
+            return currResult # // there is no next result to pre-process
+        idx = self.ch2Idx[self.st.pop()] # // we need to add elements after this idx
+        self.result = self.result[:-1]
+        
+        while len(self.st) != self.combLength:
+            idx += 1
+            temp = self.s[idx]
+            self.result += temp
+            self.st.append(temp)
+        print(currResult, self.result, self.st)
+        return currResult
+
+    def hasNext(self):
+        return len(self.st) > 0
+
+         
 
 if __name__ == "__main__":
+    '''
     itr = CombinationIterator("abcd", 2)
+    print(itr.next())     # return "ab"
+    print(itr.hasNext())  # return True
+    print(itr.next())     # return "ac"
+    print(itr.hasNext())  # return True
+    print(itr.next())     # return "bc"
+    print(itr.hasNext())  # return False
+    '''
+    itr = CombinationIterator2("abc", 2)
     print(itr.next())     # return "ab"
     print(itr.hasNext())  # return True
     print(itr.next())     # return "ac"
