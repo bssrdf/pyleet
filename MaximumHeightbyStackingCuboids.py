9671cfed17e3fdc177f8ce7a6455b9442b732418
bssrdf <bssrdf@gmail.com>
2021-11-21 16:57:00 -0500

    added a new hard problem like LIS

---
+++ MaximumHeightbyStackingCuboids.py
@@ -0,0 +1,65 @@
+'''
+-Hard-
+
+Given n cuboids where the dimensions of the ith cuboid is 
+cuboids[i] = [widthi, lengthi, heighti] (0-indexed). Choose a 
+subset of cuboids and place them on each other.
+
+You can place cuboid i on cuboid j if widthi <= widthj 
+and lengthi <= lengthj and heighti <= heightj. You can rearrange 
+any cuboid's dimensions by rotating it to put it on another cuboid.
+
+Return the maximum height of the stacked cuboids.
+
+ 
+
+Example 1:
+
+
+
+Input: cuboids = [[50,45,20],[95,37,53],[45,23,12]]
+Output: 190
+Explanation:
+Cuboid 1 is placed on the bottom with the 53x37 side facing down with height 95.
+Cuboid 0 is placed next with the 45x20 side facing down with height 50.
+Cuboid 2 is placed next with the 23x12 side facing down with height 45.
+The total height is 95 + 50 + 45 = 190.
+Example 2:
+
+Input: cuboids = [[38,25,45],[76,35,3]]
+Output: 76
+Explanation:
+You can't place any of the cuboids on the other.
+We choose cuboid 1 and rotate it so that the 35x3 side is facing down and its height is 76.
+Example 3:
+
+Input: cuboids = [[7,11,17],[7,17,11],[11,7,17],[11,17,7],[17,7,11],[17,11,7]]
+Output: 102
+Explanation:
+After rearranging the cuboids, you can see that all cuboids have the same dimension.
+You can place the 11x7 side down on all cuboids so their heights are 17.
+The maximum height of stacked cuboids is 6 * 17 = 102.
+ 
+
+Constraints:
+
+n == cuboids.length
+1 <= n <= 100
+1 <= widthi, lengthi, heighti <= 100
+'''
+
+from typing import List
+
+class Solution:
+    def maxHeight(self, cuboids: List[List[int]]) -> int:
+        A = [[0,0,0]] + sorted(map(sorted, cuboids))
+        dp = [0]*len(A)
+        for i in range(1, len(A)):
+            for j in range(i):
+                if all(A[i][k] >= A[j][k] for k in range(3)):
+                    dp[i] = max(dp[i], dp[j]+A[i][2])
+        return max(dp)
+
+        
+if __name__ == "__main__":
+    print(Solution().maxHeight([[50,45,20],[95,37,53],[45,23,12]]))
