4332687f6069945a8187f786ea91dde2ea587591
bssrdf <merlintiger@hotmail.com>
2022-03-29 09:49:29 -0400

    add another solution

--- GCDSortofanArray.py
+++ GCDSortofanArray.py
@@ -46,6 +46,20 @@ from typing import List
 from collections import defaultdict, Counter
 import math
 
+class UnionFind:
+    def __init__(self):
+        self.parent = {}
+
+    def find(self, u):
+        if u != self.parent.setdefault(u, u):
+            self.parent[u] = self.find(self.parent[u])  # Path compression
+        return self.parent[u]
+
+    def union(self, u, v):
+        pu, pv = self.find(u), self.find(v)
+        if pu != pv: self.parent[pu] = pv
+
+
 class Solution:
     def gcdSort(self, nums: List[int]) -> bool:
         n = len(nums)
@@ -94,18 +108,43 @@ class Solution:
             idx = graph[k]
             for i in range(len(idx)-1):
                 union(idx[i], idx[i+1])
+        # print(roots)
+        cnt = defaultdict(list)
         for i in range(n):
-            if find(i) != find(sortIndex[i]):
+            cnt[find(i)].append(i)        
+        for r in cnt:
+            match = set()
+            for j in cnt[r]:
+                match.add(sortIndex[j])
+            if match != set(cnt[r]):
                 return False
-        # cnt = defaultdict(list)
-        # for i in range(n):
-        #     cnt[find(i)].append(i)        
-        # for r in cnt:
-        #     match = set()
-        #     for j in cnt[r]:
-        #         match.add(sortIndex[j])
-        #     if match != set(cnt[r]):
-        #         return False
+        return True
+        
+    def gcdSort(self, nums: List[int]) -> bool:
+        def sieve(n):  # O(N*log(logN)) ~ O(N)
+            spf = [i for i in range(n)]
+            for i in range(2, n):
+                if spf[i] != i: continue  # Skip if it's a not prime number
+                for j in range(i * i, n, i):
+                    if spf[j] > i:  # update to the smallest prime factor of j
+                        spf[j] = i
+            return spf
+
+        def getPrimeFactors(num, spf):  # O(logNum)
+            while num > 1:
+                yield spf[num]
+                num //= spf[num]
+
+        spf = sieve(max(nums) + 1)
+        uf = UnionFind()
+        for x in nums:
+            for f in getPrimeFactors(x, spf):
+                uf.union(x, f)
+
+        for x, y in zip(nums, sorted(nums)):
+            if uf.find(x) != uf.find(y):
+                return False
+
         return True
 
 
