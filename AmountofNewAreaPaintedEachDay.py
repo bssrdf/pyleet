70b3ad9ce57d6dad0cf49a1cfaec2c57787ccb2d
bssrdf <merlintiger@hotmail.com>
2022-04-04 21:41:50 -0400

    updated Fenwick Tree and two problems with intervals

---
+++ AmountofNewAreaPaintedEachDay.py
@@ -0,0 +1,242 @@
+'''
+-Hard-
+
+
+There is a long and thin painting that can be represented by a number line. 
+You are given a 0-indexed 2D integer array paint of length n, where 
+paint[i] = [starti, endi]. This means that on the ith day you need to paint 
+the area between starti and endi.
+
+Painting the same area multiple times will create an uneven painting so you 
+only want to paint each area of the painting at most once.
+
+Return an integer array worklog of length n, where worklog[i] is the amount 
+of new area that you painted on the ith day.
+
+ 
+
+Example 1:
+
+
+
+Input: paint = [[1,4],[4,7],[5,8]]
+Output: [3,3,1]
+Explanation:
+On day 0, paint everything between 1 and 4.
+The amount of new area painted on day 0 is 4 - 1 = 3.
+On day 1, paint everything between 4 and 7.
+The amount of new area painted on day 1 is 7 - 4 = 3.
+On day 2, paint everything between 7 and 8.
+Everything between 5 and 7 was already painted on day 1.
+The amount of new area painted on day 2 is 8 - 7 = 1. 
+Example 2:
+
+
+
+Input: paint = [[1,4],[5,8],[4,7]]
+Output: [3,3,1]
+Explanation:
+On day 0, paint everything between 1 and 4.
+The amount of new area painted on day 0 is 4 - 1 = 3.
+On day 1, paint everything between 5 and 8.
+The amount of new area painted on day 1 is 8 - 5 = 3.
+On day 2, paint everything between 4 and 5.
+Everything between 5 and 7 was already painted on day 1.
+The amount of new area painted on day 2 is 5 - 4 = 1. 
+Example 3:
+
+
+
+Input: paint = [[1,5],[2,4]]
+Output: [4,0]
+Explanation:
+On day 0, paint everything between 1 and 5.
+The amount of new area painted on day 0 is 5 - 1 = 4.
+On day 1, paint nothing because everything between 2 and 4 was already painted on day 0.
+The amount of new area painted on day 1 is 0.
+ 
+
+Constraints:
+
+1 <= paint.length <= 105
+paint[i].length == 2
+0 <= starti < endi <= 5 * 104
+
+
+'''
+
+from typing import List
+import collections
+import heapq
+
+class Node:
+    def __init__(self, l, r):
+        self.left = None
+        self.right = None
+        self.l = l
+        self.r = r
+        self.mid = (l + r) >> 1
+        self.v = 0
+        self.add = 0
+
+
+class SegmentTree:
+    def __init__(self):
+        self.root = Node(1, 10**5 + 10)
+
+    def modify(self, l, r, v, node=None):
+        if l > r:
+            return
+        if node is None:
+            node = self.root
+        if node.l >= l and node.r <= r:
+            node.v = node.r - node.l + 1
+            node.add = v
+            return
+        self.pushdown(node)
+        if l <= node.mid:
+            self.modify(l, r, v, node.left)
+        if r > node.mid:
+            self.modify(l, r, v, node.right)
+        self.pushup(node)
+
+    def query(self, l, r, node=None):
+        if l > r:
+            return 0
+        if node is None:
+            node = self.root
+        if node.l >= l and node.r <= r:
+            return node.v
+        self.pushdown(node)
+        v = 0
+        if l <= node.mid:
+            v += self.query(l, r, node.left)
+        if r > node.mid:
+            v += self.query(l, r, node.right)
+        return v
+
+    def pushup(self, node):
+        node.v = node.left.v + node.right.v
+
+    def pushdown(self, node):
+        if node.left is None:
+            node.left = Node(node.l, node.mid)
+        if node.right is None:
+            node.right = Node(node.mid + 1, node.r)
+        if node.add:
+            left, right = node.left, node.right
+            left.v = left.r - left.l + 1
+            right.v = right.r - right.l + 1
+            left.add = node.add
+            right.add = node.add
+            node.add = 0
+
+
+
+class Solution:
+    def amountPainted(self, paint: List[List[int]]) -> List[int]:
+        m = len(paint)
+        n = 5*10**4+1
+        n = 23
+        B1 = [0]*n
+        B2 = [0]*n
+        def query(BIT, x):
+            sums = 0
+            while x > 0:
+                sums += BIT[x]
+                x -= x & (-x)
+            return sums 
+        def update(BIT, x, i):
+            while x < n:                
+                BIT[x] += i
+                x += x & (-x)
+            
+        def range_update(l, r, x):
+            update(B1, l, x)
+            update(B1, r+1, -x)
+            update(B2, l, x*(l-1))
+            update(B2, r+1, -x*r)
+
+            
+
+        def prefix_sum(idx):
+            return query(B1, idx)*idx - query(B2, idx)
+
+        def range_query(l, r):
+            return prefix_sum(r-1) - prefix_sum(l-1)
+
+        ans = [0]*m
+        for i,(s,e) in enumerate(paint):  
+            a = range_query(s,e)          
+            ans[i] = (e-s) - range_query(s, e)
+            range_update(s,e-1,1)
+            print(s, e, a, B1, B2)
+        return ans    
+    
+    def amountPainted2(self, paint: List[List[int]]) -> List[int]:
+        tree = SegmentTree()
+        ans = []
+        for i, (start, end) in enumerate(paint):
+            l, r = start + 1, end
+            v = tree.query(l, r)
+            ans.append(r - l + 1 - v)
+            tree.modify(l, r, 1)
+        return ans
+
+    def amountPainted3(self, paint):
+        """
+        :type paint: List[List[int]]
+        :rtype: List[int]
+        """
+        points = collections.defaultdict(list)
+        for i, (s, e) in enumerate(paint):
+            points[s].append((True, i))
+            points[e].append((False, i))
+        min_heap = []
+        lookup = [False]*len(paint)
+        result = [0]*len(paint)
+        prev = -1
+        for pos in sorted(points.keys()):
+            while min_heap and lookup[min_heap[0]]:
+                heapq.heappop(min_heap)
+            if min_heap:
+                result[min_heap[0]] += pos-prev
+            prev = pos
+            for t, i in points[pos]:
+                if t:
+                    heapq.heappush(min_heap, i)
+                else:
+                    lookup[i] = True
+        return result
+     
+import random
+if __name__ == "__main__":
+    # paint = [[1,4],[4,7],[5,8]]
+    paint = [[14,18],[12,16],[4,7],[3,22]]
+    print(Solution().amountPainted(paint))
+    # paint = [[1,4],[5,8],[4,7]]
+    # print(Solution().amountPainted(paint))
+    # paint = [[1,5],[2,4]]
+    # print(Solution().amountPainted(paint))
+
+
+    # paint = [[1,10],[20,34], [8, 12], [16,19]]
+    # print(Solution().amountPainted(paint))
+    # print(Solution().amountPainted2(paint))
+    # N, segs = 1000, set()
+    # while len(segs) < N:
+    #     i, j = random.randint(1,1000), random.randint(1,1000)
+    #     if i < j:
+    #         segs.add((i,j))
+    # paint = list(segs)
+    # print(paint)
+    # paint = [(73, 493), (454, 900), (204, 270), (54, 941)]
+    # print(Solution().amountPainted(paint))
+    # print(Solution().amountPainted2(paint))
+
+    
+
+
+
+
+
--- FenwickTree.py
+++ FenwickTree.py
@@ -36,33 +36,81 @@ class FenwickTree(object):
         return self.sum(x)
 
 
+class FenwickTreeRURQ(object):
+    def __init__(self, n):
+        self.B1 = [0] * (n + 1)
+        self.B2 = [0] * (n + 1)
+        self.n = n
+ 
+    def lowbit(self, x):
+        return x & -x
+ 
+    def add(self, B, x, val):
+        while x <= self.n:
+            B[x] += val
+            x += self.lowbit(x)
     
-    
+    def update(self, B, x, val):
+        self.add(B, x, val)
+
+    def range_update(self, ui, uj, val):
+        self.update(self.B1, ui, val)
+        self.update(self.B1, uj+1, -val)
+        self.update(self.B2, ui, val*(ui-1))
+        self.update(self.B2, uj+1, -val*uj)
+ 
+    def query(self, B, x):
+        res = 0
+        while x > 0:
+            res += B[x]
+            x -= self.lowbit(x)
+        return res
+
+
+    def prefix_sum(self, idx):
+        return self.query(self.B1, idx)*idx - self.query(self.B2, idx)
+
+    def range_query(self, l, r):
+        return self.prefix_sum(r) - self.prefix_sum(l-1)
+
 
 import bisect 
 
 if __name__ == "__main__":
     n = 7
     #print bin(n)
-    print(FenwickTree(10).lowbit(n))
-    # Fenick tree can be used for dynamic query of number of values within a range
-    arr = [8, 1, 0, -1, 2, -2, 5]
-    # query: q[i] is how many numbers <= x to the left of index i
-    x = 6
-    sortA = sorted(arr)
-    bit = FenwickTree(len(arr))
-    q = []
-    for a in arr: 
-        bit.add(bisect.bisect_left(sortA, a)+1, 1)
-        q.append(bit.sum(bisect.bisect_right(sortA, x)))
-        #bit.add(bisect.bisect_left(sortA, a)+1, 1)
-    print(q) 
+    # print(FenwickTree(10).lowbit(n))
+    # # Fenick tree can be used for dynamic query of number of values within a range
+    # arr = [8, 1, 0, -1, 2, -2, 5]
+    # # query: q[i] is how many numbers <= x to the left of index i
+    # x = 6
+    # sortA = sorted(arr)
+    # bit = FenwickTree(len(arr))
+    # q = []
+    # for a in arr: 
+    #     bit.add(bisect.bisect_left(sortA, a)+1, 1)
+    #     q.append(bit.sum(bisect.bisect_right(sortA, x)))
+    #     #bit.add(bisect.bisect_left(sortA, a)+1, 1)
+    # print(q) 
 
     rupq = FenwickTree(10)
-    rupq.range_update(2,9,7)
-    rupq.range_update(6,7,3)
-    for i in range(1, 11):        
-        print(i, rupq.point_query(i))
+    rupq.range_update(2,9,1)
+    rupq.range_update(6,7,1)
+    print(rupq.point_query(6))
+
+    # for i in range(1, 11):        
+    #     print(i, rupq.point_query(i))
+
+    rurq = FenwickTreeRURQ(10)
+    rurq.range_update(2,9,7)
+    rurq.range_update(6,7,3)
+    print(rurq.range_query(1,10))
+    print(rurq.range_query(6,7))
+    
+
+
+
+
 
 
 
