3d74381725297e3a66fab3897e948ed48332db33
bssrdf <merlintiger@hotmail.com>
2022-04-26 21:42:02 -0400

    a few mediums and hards

--- SwapAdjacentinLRString.py
+++ SwapAdjacentinLRString.py
@@ -50,6 +50,30 @@ class Solution:
                 if dp[i-1]: dp[i] = True
         # print(dp)
         return dp[n]        
+
+    def canTransform2(self, start: str, end: str) -> bool:
+        if len(start) != len(end): return False
+        
+        # check L R orders are the same
+        if start.replace('X','') != end.replace('X', ''): return False
+        
+        n = len(start)
+        Lstart = [i for i in range(n) if start[i] == 'L']
+        Lend = [i for i in range(n) if end[i] == 'L']
+        
+        Rstart = [i for i in range(n) if start[i] == 'R']
+        Rend = [i for i in range(n) if end[i] == 'R']
+		# check L positions are correct
+        for i, j in zip(Lstart, Lend):
+            if i < j:
+                return False
+            
+        # check R positions are correct
+        for i, j in zip(Rstart, Rend):
+            if i > j:
+                return False
+            
+        return True
                 
 
 
