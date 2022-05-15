966ef8f2176e568b59cecd5b6265a6213e1dda39
bssrdf <merlintiger@hotmail.com>
2022-05-15 13:36:39 -0400

    add an O(N) two pointers solution

--- MaximumWhiteTilesCoveredbyaCarpet.py
+++ MaximumWhiteTilesCoveredbyaCarpet.py
@@ -1,4 +1,5 @@
 
+from operator import itemgetter
 from typing import List
 import bisect
 class Solution:
@@ -18,7 +19,8 @@ class Solution:
         for i in range(n):
             l = T[i][0] + carpetLen
             
-            idx = bisect.bisect_left(arr, (l, 0))            
+            # idx = bisect.bisect_left(arr, (l, 0))            
+            idx = bisect.bisect_left(arr, l, key=itemgetter(0))            
 
             if idx <len(arr) and arr[idx-1][1] == arr[idx][1]:
                 sums =  preSum[arr[idx-1][1]] - preSum[i]          
@@ -30,11 +32,33 @@ class Solution:
 
         return ans     
 
+    def maximumWhiteTiles2(self, tiles: List[List[int]], carpetLen: int) -> int:
+        T, n = tiles, len(tiles)
+        tiles.sort()
+        preSum = [0]*(n+1)
+        ans =  0
+        for i in range(n):
+            preSum[i+1] = preSum[i] + T[i][1]-T[i][0]+1
+        j = 0
+        for i in range(n):
+            k = tiles[i][0]+carpetLen-1
+            while j < n and k >= tiles[j][1]:
+                j += 1
+            l = preSum[j] - (0 if i==0 else preSum[i])
+            if j < n:
+                l += max(0, k - tiles[j][0]+1)
+            ans = max(ans, l)            
+        return ans
+
+
+
 
 
 
 if __name__ == "__main__":
     print(Solution().maximumWhiteTiles(tiles = [[1,5],[10,11],[12,18],[20,25],[30,32]], carpetLen = 10))     
     print(Solution().maximumWhiteTiles(tiles = [[10,11],[1,1]], carpetLen = 2))     
+    print(Solution().maximumWhiteTiles2(tiles = [[1,5],[10,11],[12,18],[20,25],[30,32]], carpetLen = 10))     
+    print(Solution().maximumWhiteTiles2(tiles = [[10,11],[1,1]], carpetLen = 2))     
 
         
