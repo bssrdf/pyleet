128bf69e0717c08e8bff83d30e7c89eadec5aec1
bssrdf <bssrdf@gmail.com>
2022-05-16 22:55:59 -0400

    add a new tag

--- KthAncestorofaTreeNode.py
+++ KthAncestorofaTreeNode.py
@@ -2,6 +2,8 @@
 
 -Hard-
 
+*Binary Lifting*
+
 You are given a tree with n nodes numbered from 0 to n - 1 in the form of a parent array parent where 
 parent[i] is the parent of ith node. The root of the tree is node 0. Find the kth ancestor of a given node.
 
