81b6d942eb93498783329f54711cbba4b047fff2
bssrdf <bssrdf@gmail.com>
2022-06-07 21:22:09 -0400

    update with O(n) space 01 knapsack solution

--- MaximumProfitFromTradingStocks.py
+++ MaximumProfitFromTradingStocks.py
@@ -1,6 +1,7 @@
 '''
 -Medium-
 *DP*
+*Knapsack Problem*
 
 You are given two 0-indexed integer arrays of the same length present and future where present[i] is the current price of the ith stock and future[i] is the price of the ith stock a year in the future. You may buy each stock at most once. You are also given an integer budget representing the amount of money you currently have.
 
@@ -64,6 +65,7 @@ class Solution(object):
         return max(dp[n])
     
     def maxProfits2(self, present, future, budget):
+        # stack overflow
         P, F, B = present, future, budget
         pos = [(v,i) for i,v in enumerate(P)]
         pos.sort(key = lambda x: x[0])
@@ -77,12 +79,20 @@ class Solution(object):
         def dp(i, m):
             if i == n: return 0
             if P[i] > m: return 0
-            # if m == 0: return 0
+            if m <= 0: return 0
             ans = dp(i+1, m) 
             x = F[i]-P[i]
             ans = max(ans, x + dp(i+1, m-P[i]))            
             return ans 
         return dp(0, B)
+
+    def maxProfits3(self, present, future, budget):
+        arr = [(a, b - a) for a, b in zip(present, future) if b > a]
+        dp = [0] * (budget + 1)
+        for v, w in arr:
+            for j in range(budget, v - 1, -1):
+                dp[j] = max(dp[j], dp[j - v] + w)
+        return dp[-1]
 
 
 
@@ -90,6 +100,7 @@ class Solution(object):
 
 
 if __name__ == "__main__":   
+    from random import randint
     print(Solution().maxProfits(present = [5,4,6,2,3], future = [8,5,4,3,5], budget = 10))
     print(Solution().maxProfits(present = [2,2,5], future = [3,4,10], budget = 6))
     print(Solution().maxProfits(present = [3,3,12], future = [0,3,15], budget = 10))
@@ -101,4 +112,18 @@ if __name__ == "__main__":
     print(Solution().maxProfits2(present = [3,3,12], future = [0,3,15], budget = 10))
     print(Solution().maxProfits2(present = [3, 6, 10, 7, 18, 25, 3, 12], 
                                  future =  [0, 10, 6, 5, 27, 14, 13, 15], budget = 21))
+    
+    n = 1000
+    present, future = [], []
+    for i in range(n):
+        p, f = randint(0, 100), randint(0, 100)
+        present.append(p)
+        future.append(f)
+    print(Solution().maxProfits2(present = present,
+                                 future =  future, budget = 887))
+    print(Solution().maxProfits3(present = present,
+                            future =  future, budget = 887))
+    
+
+
     
