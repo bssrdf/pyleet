79a8472c9192ae43800067dbdac2ca47e52abcaa
bssrdf <merlintiger@hotmail.com>
2022-06-20 20:39:02 -0400

    add a DP solution

--- LongestBinarySubsequenceLessThanorEqualtoK.py
+++ LongestBinarySubsequenceLessThanorEqualtoK.py
@@ -2,7 +2,7 @@
 
 -Medium-
 *Greedy*
-
+*DP*
 
 You are given a binary string s and a positive integer k.
 
@@ -49,7 +49,50 @@ class Solution:
             if val + pow > k: break
         return s.count('0') + cnt
 
+    
+    def longestSubsequence2(self, s: str, k: int) -> int:
+        n = len(s)
+        # dp[i] denotes the minimum binary value of a subsequence of length i
+        dp = [0x3f3f3f3f]*(n+1)         
+        dp[0] = 0
+        dp[1] = int(s[0] == '1')
+        for i in range(1, n):
+            for j in range(i, -1, -1):
+                cur = (dp[j] << 1) + int(s[i] == '1')
+                if cur < dp[j+1]:
+                    dp[j+1] = cur
+        ret = 1
+        for i in range(2, n+1):
+            if dp[i] <= k: ret = i
+        return ret
+
+    def longestSubsequence3(self, s: str, k: int) -> int:
+        # TLE
+        # dp[i][j] denotes the minimum value of a subsequence of j length 
+        # using values upto 0 to i index of array.
+        # i -> index of array
+        # j -> len of subsequence
+        n, INT_MAX = len(s), 0x3f3f3f3f
+        dp = [[INT_MAX]*(n+1)  for _ in range(n+1)]
+        for i in range(n+1):
+            dp[i][0] = 0
+        for i in range(1, n+1):
+            for j in range(1, i+1):
+                dp[i][j] = min(dp[i-1][j],dp[i][j])
+                if dp[i-1][j-1] != INT_MAX:
+                    dp[i][j] = min(dp[i][j],dp[i-1][j-1]*2+int(s[i-1]=='1'))
+        ret = 1
+        for i in range(1, n+1):
+            for j in range(n+1):
+                if dp[i][j] <= k: 
+                    ret = max(ret, j)
+        return ret            
+        
+
+
 
 if __name__ == "__main__":
     print(Solution().longestSubsequence(s = "1001010", k = 5))
     print(Solution().longestSubsequence(s = "00101001", k = 1))
+    print(Solution().longestSubsequence2(s = "1001010", k = 5))
+    print(Solution().longestSubsequence2(s = "00101001", k = 1))
