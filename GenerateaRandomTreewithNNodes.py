fb2957ba8b4e4fbeb880564d5a30832d06aa3b73
bssrdf <bssrdf@gmail.com>
2022-08-19 23:37:28 -0400

    added a faster O(NlogN) method to generate tree using Prufer's code

--- GenerateaRandomTreewithNNodes.py
+++ GenerateaRandomTreewithNNodes.py
@@ -11,9 +11,11 @@ Generate a uniformly distributed random labelled trees with N nodes.
 from typing import List
 from collections import defaultdict
 from random import random, randint
+from sortedcontainers import SortedSet
 
 class Solution:
     def generateTree(self, n: int) -> List[List[int]]:
+        # O(N^2) from wikipedia
         self.n = n
         # generate a random Prufer sequence of length n-2
         prufer = [randint(1, n) for i in range(n-2)]
@@ -41,6 +43,31 @@ class Solution:
         degree[u] -= 1
         degree[v] -= 1
         return edges
+        
+        
+    def generateTreeFast(self, n: int) -> List[List[int]]:
+        # O(NlogN) 
+        # https://forthright48.com/prufer-code-linear-representation-of-a-labeled-tree/
+        self.n = n
+        prufer = [randint(1, n) for i in range(n-2)]
+        nodeCount = defaultdict(int)
+        leaves = SortedSet()
+        edges = []
+        for i in range(len(prufer)):
+           nodeCount[prufer[i]] += 1
+        # Find the absent nodes
+        for i in range(1, n+1):
+            if i not in nodeCount: leaves.add(i)
+        for i in range(len(prufer)):
+            a = prufer[i]
+            b = leaves.pop()
+            edges.append([a,b])
+            nodeCount[a] -= 1
+            if nodeCount[a] == 0:
+                leaves.add(a)
+        edges.append([leaves[0], leaves[-1]])
+        return edges     
+        
     
 
     def swapRoot(self, edges: List[List[int]]) -> List[int]:
@@ -83,6 +110,8 @@ if __name__ == "__main__":
     n = 20
     sol = Solution()
     tree = sol.generateTree(n)
+    # tree = sol.generateTreeFast(n)
+
     tree = sol.swapRoot(tree) 
     print(tree)
     # deg = [0]*(n)
