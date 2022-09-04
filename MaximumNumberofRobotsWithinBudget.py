d6bb095f973b40a59e1a7bf805540d3284a1c424
bssrdf <merlintiger@hotmail.com>
2022-09-04 00:18:03 -0400

    2 from today's contest

---
+++ MaximumNumberofRobotsWithinBudget.py
@@ -0,0 +1,94 @@
+'''
+
+You have n robots. You are given two 0-indexed integer arrays, chargeTimes and runningCosts, both of length n. The ith robot costs chargeTimes[i] units to charge and costs runningCosts[i] units to run. You are also given an integer budget.
+
+The total cost of running k chosen robots is equal to max(chargeTimes) + k * sum(runningCosts), where max(chargeTimes) is the largest charge cost among the k robots and sum(runningCosts) is the sum of running costs among the k robots.
+
+Return the maximum number of consecutive robots you can run such that the total cost does not exceed budget.
+
+ 
+
+Example 1:
+
+Input: chargeTimes = [3,6,1,3,4], runningCosts = [2,1,3,4,5], budget = 25
+Output: 3
+Explanation: 
+It is possible to run all individual and consecutive pairs of robots within budget.
+To obtain answer 3, consider the first 3 robots. The total cost will be max(3,6,1) + 3 * sum(2,1,3) = 6 + 3 * 6 = 24 which is less than 25.
+It can be shown that it is not possible to run more than 3 consecutive robots within budget, so we return 3.
+Example 2:
+
+Input: chargeTimes = [11,12,19], runningCosts = [10,8,7], budget = 19
+Output: 0
+Explanation: No robot can be run that does not exceed the budget, so we return 0.
+ 
+
+Constraints:
+
+chargeTimes.length == runningCosts.length == n
+1 <= n <= 5 * 104
+1 <= chargeTimes[i], runningCosts[i] <= 105
+1 <= budget <= 1015
+
+
+'''
+from typing import List
+from collections import deque
+
+class Solution:
+    def maximumRobots(self, chargeTimes: List[int], runningCosts: List[int], budget: int) -> int:
+        #O(NlogN)
+        C, R, n = chargeTimes, runningCosts, len(runningCosts)
+        l, r = 0, n+1
+        preSum = [0]*(n+1)
+        for i in range(n):
+            preSum[i+1] = preSum[i] + R[i]  
+        def canRun(k):
+            if k == 0: return True
+            dq = deque()
+            for i in range(n):
+                j = i - k + 1
+                while dq and C[dq[-1]] < C[i]:
+                    dq.pop()            
+                dq.append(i)
+                if i >= k and dq[0] < j:
+                    dq.popleft()
+                if j >= 0 and C[dq[0]] + k*(preSum[i+1]-preSum[j]) <= budget:
+                    return True 
+            return False 
+
+        while l < r:
+            mid = l + (r-l)//2
+            if canRun(mid):
+                l = mid + 1
+            else:
+                r = mid
+        return l-1
+
+    def maximumRobots2(self, times: List[int], costs: List[int], budget: int) -> int:
+        # O(N)
+        cur = i = 0
+        n = len(times)
+        d = deque()
+        for j in range(n):
+            cur += costs[j]
+            while d and times[d[-1]] <= times[j]:
+                d.pop()
+            d.append(j)
+            if times[d[0]] + (j - i + 1) * cur > budget:
+                if d[0] == i:
+                    d.popleft()
+                cur -= costs[i]
+                i += 1
+        return n - i
+        
+
+
+
+
+if __name__ == "__main__":
+    print(Solution().maximumRobots(chargeTimes = [3,6,1,3,4], runningCosts = [2,1,3,4,5], budget = 25))
+    print(Solution().maximumRobots(chargeTimes = [11,12,19], runningCosts = [10,8,7], budget = 19))
+    C = [32,83,96,70,98,80,30,42,63,67,49,10,80,13,69,91,73,10]
+    R = [49,67,92,26,18,22,38,34,36,26,32,84,39,42,88,51,8,2]
+    print(Solution().maximumRobots(chargeTimes = C, runningCosts= R, budget=599))
---
+++ MaximumRowsCoveredbyColumns.py
@@ -0,0 +1,84 @@
+'''
+-Medium-
+
+You are given a 0-indexed m x n binary matrix mat and an integer cols, which denotes the number of columns you must choose.
+
+A row is covered by a set of columns if each cell in the row that has a value of 1 also lies in one of the columns of the chosen set.
+
+Return the maximum number of rows that can be covered by a set of cols columns.
+
+ 
+
+Example 1:
+
+
+
+Input: mat = [[0,0,0],[1,0,1],[0,1,1],[0,0,1]], cols = 2
+Output: 3
+Explanation:
+As shown in the diagram above, one possible way of covering 3 rows is by selecting the 0th and 2nd columns.
+It can be shown that no more than 3 rows can be covered, so we return 3.
+Example 2:
+
+
+
+Input: mat = [[1],[0]], cols = 1
+Output: 2
+Explanation:
+Selecting the only column will result in both rows being covered, since the entire matrix is selected.
+Therefore, we return 2.
+ 
+
+Constraints:
+
+m == mat.length
+n == mat[i].length
+1 <= m, n <= 12
+mat[i][j] is either 0 or 1.
+1 <= cols <= n
+
+
+'''
+
+from typing import List
+from functools import lru_cache
+
+class Solution:
+    def maximumRows(self, mat: List[List[int]], cols: int) -> int:
+        m, n = len(mat), len(mat[0])
+        # mask = [0]*n
+        # for j in range(n):
+        #     for i in range(m):
+        #         if mat[i][j] == 1:
+        #             mask[j] |= 1 << i 
+        rows = [int(''.join(str(c) for c in r), 2) for r in mat]
+        # print(rows)
+        ans = 0
+        @lru_cache(None) 
+        def backtrack(cur, msk):
+            nonlocal ans
+            # if cur == n:
+            if bin(msk).count('1') == cols:
+                t = 0
+                for i in rows:
+                    if (i | msk) == msk: t += 1 
+                    # print(f'{i:03b}', f'{msk:03b}', t)
+                # print(f'{msk:02b}', t)    
+                ans = max(ans, t)
+                return    
+            for i in range(cur, n):
+                # if msk & (1 << i) == 0:
+                backtrack(i+1, msk | 1<<i)
+                backtrack(i+1, msk)
+
+        backtrack(0, 0)    
+        return ans
+
+
+
+if __name__ == "__main__":
+    print(Solution().maximumRows(mat = [[0,0,0],[1,0,1],[0,1,1],[0,0,1]], cols = 2))
+    print(Solution().maximumRows(mat = [[1],[0]], cols = 1))
+    print(Solution().maximumRows(mat = [[1,0,1,0],[1,0,1,0],[0,1,1,1],[0,1,0,0]], cols=3))
+    print(Solution().maximumRows(mat = [[0,0],[0,1],[0,0],[1,1]], cols = 1))
+        
