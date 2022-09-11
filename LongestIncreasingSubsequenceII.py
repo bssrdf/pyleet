88897ae6baa914a92ebbf35602d1f03584a6dcb5
bssrdf <merlintiger@hotmail.com>
2022-09-11 00:56:02 -0400

    updated with solution to problem 4

--- LongestIncreasingSubsequenceII.py
+++ LongestIncreasingSubsequenceII.py
@@ -7,6 +7,34 @@
 
 from typing import List
 
+
+class SEG:
+    def __init__(self, n):
+        self.n = n
+        self.tree = [0] * 2 * self.n
+       
+    def query(self, l, r):
+        l += self.n
+        r += self.n
+        ans = 0
+        while l < r:
+            if l & 1:
+                ans = max(ans, self.tree[l])
+                l += 1
+            if r & 1:
+                r -= 1
+                ans = max(ans, self.tree[r])
+            l >>= 1
+            r >>= 1
+        return ans
+    
+    def update(self, i, val):
+        i += self.n
+        self.tree[i] = val
+        while i > 1:
+            i >>= 1
+            self.tree[i] = max(self.tree[i * 2], self.tree[i * 2 + 1])
+
 class Solution:
     def lengthOfLIS(self, nums: List[int], k: int) -> int:
         A = nums
@@ -53,9 +81,15 @@ class Solution:
     
     def lengthOfLIS3(self, nums: List[int], k: int) -> int:
         A = nums
-        n = len(A)
-        dp = [1]*max(A)
-        
+        n, ans = max(A), 1
+        seg = SEG(n)
+        for a in A:
+            a -= 1
+            premax = seg.query(max(0, a - k), a)
+            ans = max(ans, premax + 1)
+            seg.update(a, premax + 1)
+        return ans
+
         
 
         
