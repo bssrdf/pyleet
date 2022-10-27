cc9212c2f6c76a6f474f3ba7417ebc752ca5989d
bssrdf <merlintiger@hotmail.com>
2022-10-26 20:41:22 -0400

    simplify and improve performance

--- ContinuousSubarraySum.py
+++ ContinuousSubarraySum.py
@@ -60,10 +60,33 @@ class Solution(object):
 
         return False
 
+    def checkSubarraySum2(self, nums, k):
+        p = defaultdict(int)
+        n = len(nums)
+        S = 0
+        p[S] = 0 
+        
+        for i in range(1,n+1):
+            S += nums[i-1]
+            t = S%k if k != 0 else S
+            if t in p and i-p[t]>=2:              
+                return True
+            if t in p:
+                p[t] = min(p[t], i) 
+            else:
+                p[t] = i
+        return False
+
+
 if __name__ == "__main__":
     print(Solution().checkSubarraySum([23, 2, 4, 6, 7], 6))
+    print(Solution().checkSubarraySum2([23, 2, 4, 6, 7], 6))
     print(Solution().checkSubarraySum([23, 2, 4, 6, 7], 0))
+    print(Solution().checkSubarraySum2([23, 2, 4, 6, 7], 0))
     print(Solution().checkSubarraySum([0,0], 0))
+    print(Solution().checkSubarraySum2([0,0], 0))
+    print(Solution().checkSubarraySum([0,1,0,3,0,4,0,4,0], 5))
+    print(Solution().checkSubarraySum2([0,1,0,3,0,4,0,4,0], 5))
     
 
 
