2e3cde02d28f978d3140e432592285fef22bdef7
bssrdf <merlintiger@hotmail.com>
2023-02-13 19:32:46 -0500

    updated with an more efficient solution

--- C/CounttheNumberofFairPairs.py
+++ C/CounttheNumberofFairPairs.py
@@ -3,6 +3,7 @@
 -Medium-
 *Sorting*
 *Fenwick Tree*
+*Two Pointers*
 
 
 Given a 0-indexed integer array nums of size n and two integers lower and upper, return the number of fair pairs.
@@ -58,9 +59,32 @@ class Solution:
             ans += query(bisect.bisect_right(A, upper-i)) - query(bisect.bisect_left(A, lower-i))  
             update(idx+1)  
         return ans
+    
+    def countFairPairs2(self, nums: List[int], lower: int, upper: int) -> int:
+        '''
+        Because nums[i] + nums[j] == nums[j] + nums[i] the i < j condition 
+        degrades to i != j.
+        
+        '''
+        A = nums
+        A.sort()
+        def countLess(val):
+            res, j = 0, len(A) - 1
+            for i in range(len(A)):
+                while i < j and A[i] + A[j] > val:
+                    j -= 1
+                res += max(0, j-i)
+            return res
+        return countLess(upper) - countLess(lower-1)
+
+
+
+
         
 
 
 if __name__ == '__main__':
     print(Solution().countFairPairs(nums = [0,1,7,4,4,5], lower = 3, upper = 6))
     print(Solution().countFairPairs(nums = [1,7,9,2,5], lower = 11, upper = 11))
+    print(Solution().countFairPairs2(nums = [0,1,7,4,4,5], lower = 3, upper = 6))
+    print(Solution().countFairPairs2(nums = [1,7,9,2,5], lower = 11, upper = 11))
