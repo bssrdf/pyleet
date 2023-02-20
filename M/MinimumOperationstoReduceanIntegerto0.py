e06ff72cb8f01d8aced6025ab108e7c726164ae0
bssrdf <bssrdf@gmail.com>
2023-02-19 22:52:07 -0500

    Today's weekly 2nd

---
+++ M/MinimumOperationstoReduceanIntegerto0.py
@@ -0,0 +1,62 @@
+'''
+
+
+-Medium-
+
+
+You are given a positive integer n, you can do the following operation any number of times:
+
+Add or subtract a power of 2 from n.
+Return the minimum number of operations to make n equal to 0.
+
+A number x is power of 2 if x == 2i where i >= 0.
+
+ 
+
+Example 1:
+
+Input: n = 39
+Output: 3
+Explanation: We can do the following operations:
+- Add 20 = 1 to n, so now n = 40.
+- Subtract 23 = 8 from n, so now n = 32.
+- Subtract 25 = 32 from n, so now n = 0.
+It can be shown that 3 is the minimum number of operations we need to make n equal to 0.
+Example 2:
+
+Input: n = 54
+Output: 3
+Explanation: We can do the following operations:
+- Add 21 = 2 to n, so now n = 56.
+- Add 23 = 8 to n, so now n = 64.
+- Subtract 26 = 64 from n, so now n = 0.
+So the minimum number of operations is 3.
+ 
+
+Constraints:
+
+1 <= n <= 105
+
+'''
+from collections import deque
+
+class Solution:
+    def minOperations(self, n: int) -> int:
+        que = deque([(n, 0)])
+        while que:
+            x, t = que.popleft()
+            if x == 0: return t
+            b = x & (-x)
+            que.append((x + b, t+1))
+            que.append((x - b, t+1))
+        return -1
+
+            
+
+        
+
+if __name__ == '__main__':
+    print(Solution().minOperations(39))
+    print(Solution().minOperations(54))
+    print(Solution().minOperations(48760))
+
