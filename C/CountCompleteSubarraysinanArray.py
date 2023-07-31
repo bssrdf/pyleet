0d8e0bd2992e7cf6798a37d8a40ffd95add10603
bssrdf <merlintiger@hotmail.com>
2023-07-31 18:39:41 -0400

    add a sliding window O(n) solution

--- C/CountCompleteSubarraysinanArray.py
+++ C/CountCompleteSubarraysinanArray.py
@@ -1,6 +1,6 @@
 '''
 -Medium-
-
+*Sliding Window*
 You are given an array nums consisting of positive integers.
 
 We call a subarray of an array complete if the following condition is satisfied:
@@ -35,9 +35,10 @@ Constraints:
 '''
 
 from typing import List
-
+from collections import Counter
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
+        # O(N^2)
         ndis = len(set(nums))
         n = len(nums)
         ans = 0
@@ -50,11 +51,28 @@ class Solution:
                 if len(st) == ndis:
                     ans += 1
         return ans
-
-
-
-
-
+    
+    def countCompleteSubarrays2(self, nums: List[int]) -> int:
+        # O(N)
+        ndis = len(set(nums))
+        ans = 0
+        cnt = Counter()
+        j = 0
+        proc = False
+        for i,a in enumerate(nums):
+            cnt[a] += 1
+            if len(cnt) == ndis:
+                proc = True
+            while len(cnt) == ndis:
+                cnt[nums[j]] -= 1
+                if cnt[nums[j]] == 0:
+                    cnt.pop(nums[j])
+                j += 1
+            if proc:
+                ans += j  
+        return ans 
 if __name__ == "__main__":
     print(Solution().countCompleteSubarrays(nums = [1,3,1,2,2]))
+    print(Solution().countCompleteSubarrays2(nums = [1,3,1,2,2]))
     print(Solution().countCompleteSubarrays(nums = [5,5,5,5]))
+    print(Solution().countCompleteSubarrays2(nums = [5,5,5,5]))
