b9a1946194373172115648bcf6796d8736af4714
bssrdf <merlintiger@hotmail.com>
2023-09-25 15:53:33 -0400

    added another solution following hints

--- C/CountValidPathsinaTree.py
+++ C/CountValidPathsinaTree.py
@@ -156,6 +156,48 @@ class Solution:
         dfs1(1,0)    
         dfs2(1,0)     
         return ans        
+    
+    def countPaths3(self, n: int, edges: List[List[int]]) -> int:
+        def mul(x, y):
+            return x * y
+        
+        def dfs(x, f, con, prime, r):
+            v = [1 - prime[x], prime[x]]
+            for y in con[x]:
+                if y == f:
+                    continue
+                p = dfs(y, x, con, prime, r)
+                r[0] += mul(p[0], v[1]) + mul(p[1], v[0])
+                if prime[x]:
+                    v[1] += p[0]
+                else:
+                    v[0] += p[0]
+                    v[1] += p[1]
+            return v
+        
+        prime = [True] * (n + 1)
+        prime[1] = False
+        
+        all_primes = []
+        for i in range(2, n + 1):
+            if prime[i]:
+                all_primes.append(i)
+            for x in all_primes:
+                temp = i * x
+                if temp > n:
+                    break
+                prime[temp] = False
+                if i % x == 0:
+                    break
+        
+        con = [[] for _ in range(n + 1)]
+        for e in edges:
+            con[e[0]].append(e[1])
+            con[e[1]].append(e[0])
+        
+        r = [0]
+        dfs(1, 0, con, prime, r)
+        return r[0]
                
 
 
@@ -163,4 +205,6 @@ if __name__ == "__main__":
     print(Solution().countPaths(n = 5, edges = [[1,2],[1,3],[2,4],[2,5]]))
     print(Solution().countPaths(n = 6, edges = [[1,2],[1,3],[2,4],[3,5],[3,6]]))
     print(Solution().countPaths(n = 9, edges =
+[[7,4],[3,4],[5,4],[1,5],[6,4],[9,5],[8,7],[2,8]]))
+    print(Solution().countPaths3(n = 9, edges =
 [[7,4],[3,4],[5,4],[1,5],[6,4],[9,5],[8,7],[2,8]]))
