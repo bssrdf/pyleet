a028523f93900387adeb03c3d1013b51476b988d
bssrdf <merlintiger@hotmail.com>
2024-01-07 21:33:36 -0500

    add another weekly contest problem using set difference

--- M/MaximumSizeofaSetAfterRemovals.py
+++ M/MaximumSizeofaSetAfterRemovals.py
@@ -65,7 +65,7 @@ class Solution:
                 k = cnt2[c] - 1
                 d = min(k, m-t2)
                 t2 += d
-                cnt2c[c] -= cnt2[c] - d
+                cnt2c[c] = cnt2[c] - d
             if t2 == m:
                 break
         cnt2 = cnt2c    
@@ -74,14 +74,11 @@ class Solution:
         if t1 == m and t2 == m:
             return len(cnt1 | cnt2)
         elif t1 == m:
-            # while t2 < m:
-            cnt2c = Counter()
+            cnt2c = dict(cnt2)
             for c in cnt2:
                 if c in cnt1:                    
                     t2 += cnt2[c]
-                    # cnt2.pop(c)
-                else:
-                    cnt2c[c] = cnt2[c]    
+                    cnt2c.pop(c)                
                 if t2 == m:
                     break
             cnt2 = cnt2c    
@@ -115,28 +112,60 @@ class Solution:
             cnt1 = cnt1c    
             return len(cnt1 | cnt2) 
         else:
-            cnt1c = dict(cnt1)
-            for c in cnt1:
-                if c in cnt2:                    
-                    t1 += cnt1[c]
-                    cnt1c.pop(c)
-                if t1 == m:
-                    break
-            cnt1 = cnt1c    
-            cnt2c = dict(cnt2)
-            for c in cnt2:
-                if c in cnt1:                    
-                    t2 += cnt2[c]
-                    cnt2c.pop(c)
-                if t2 == m:
-                    break
-            # if t1 == m and t2 == m:
-            cnt2 = cnt2c
-            return len(cnt1 | cnt2)
+            cnt1c, cnt2c = dict(cnt1), dict(cnt2)
+            exc = set()
+            for c in cnt1:                
+                if c in cnt2 :
+                   exc.add(c) 
+                   cnt1c.pop(c)
+                   cnt2c.pop(c)   
+            a = len(cnt1c)                  
+            b = len(cnt2c)  
+            c = len(exc)
+            print(a,b,c, cnt1c, cnt2c)
+            # if c == 0:
+            if a == 0 and b == 0:
+                return c
+            print(a,b,c)
+            if a < m and b < m: 
+                for z3 in range(c):
+                    if (c - z3 + b - a) % 2 == 0:
+                        z1 = (c - z3 + b - a)//2
+                        z2 = c - z3 - z1
+                        return a+z1+b+z2 
+            elif a == m: 
+                if b >= m:
+                    return 2*m
+                return 2*min(m, b+c)
+            elif b == m: 
+                if a >= m:
+                    return 2*m
+                return 2*min(m, a+c)
+            else:
+                return 2*m
+
+    def maximumSetSize2(self, nums1: List[int], nums2: List[int]) -> int:
+        s1 = set(nums1)
+        s2 = set(nums2)
+        n = len(nums1)
+        inter = s1.intersection(s2)
+        ex1 = min(len(s1) - len(inter) , n//2)
+        ex2 = min(len(s2) - len(inter) , n//2)
+        return (min(ex1 + ex2 + len(inter) , n))
+            
 
 
 if __name__ == "__main__":
-    print(Solution().maximumSetSize(nums1 = [1,2,1,2], nums2 = [1,1,1,1]))
-    print(Solution().maximumSetSize(nums1 = [1,2,3,4,5,6], nums2 = [2,3,2,3,2,3]))
-    print(Solution().maximumSetSize(nums1 = [1,1,2,2,3,3], nums2 = [4,4,5,5,6,6]))
+    # print(Solution().maximumSetSize(nums1 = [1,2,1,2], nums2 = [1,1,1,1]))
+    # print(Solution().maximumSetSize(nums1 = [1,2,3,4,5,6], nums2 = [2,3,2,3,2,3]))
+    # print(Solution().maximumSetSize(nums1 = [1,1,2,2,3,3], nums2 = [4,4,5,5,6,6]))
         
+    # print(Solution().maximumSetSize(nums1 = [1,2,1,1], nums2 = [1,2,3,4]))
+    # print(Solution().maximumSetSize(nums1 = [8,9], nums2 = [3,4]))
+    # # print(Solution().maximumSetSize(nums1 = [10,8,7,9], nums2 = [7,9,9,5]))
+    # # print(Solution().maximumSetSize(nums1 = [2,4,1,4], nums2 = [10,2,4,10]))
+    # # print(Solution().maximumSetSize(nums1 = [7,2,5,1,3,1,4,5], nums2 = [1,5,5,9,1,6,4,1]))
+    # print(Solution().maximumSetSize(nums1 = [5,1,9,5,6,4,9,6,7,6], nums2 = [7,6,9,2,10,8,3,10,8,2]))
+    # print(Solution().maximumSetSize(nums1 = [1,10,6,5], nums2 = [3,7,10,10]))
+    # print(Solution().maximumSetSize(nums1 = [5,2,8,6], nums2 = [7,4,1,1] ))
+    print(Solution().maximumSetSize(nums1 = [1,3,3,2] , nums2 = [2,2,1,3] ))
